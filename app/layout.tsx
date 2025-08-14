a6c04a3e50148f15b3ae2fcacfee292a0aa2f003
AdOnDifference <daniel.hamzeh@icloud.com>
2025-08-14 10:44:58 +0200

    Layout

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+// app/layout.tsx
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "JU Flörsheim Umfrage",
+    description: "Umfrage für den Kommunalwahlkampf der JU Flörsheim",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="de">
+        <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
